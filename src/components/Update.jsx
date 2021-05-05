4eafc8caef947329bf22e221219069c557a3f84e
Sejin Kim <jeen0202@korea.ac.kr>
2021-05-05 23:18:58 +0900

    Update 페이지에 정보 반영

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,11 +1,29 @@
 import  React, {Component } from 'react'
 
 export default class Update extends Component {
+    state = {
+        title:this.props.title,
+        desc:this.props.desc,
+        id:this.props.id
+    }
     render() {
         return(
-            <div>
-                hi
-            </div>
+            <form onSubmit={function(e){
+                e.preventDefault();                
+                this.props.onSubmit(
+                    e.target.title.value,
+                    e.target.desc.value
+                    );
+            }.bind(this)}>
+                <input type="hidden" name ="id" value = {this.state.id}></input>
+                <p>
+                    <input type = "text" name ="title" placeholder="TITLE" value = {this.state.title}></input>
+                </p>
+                <p>
+                    <textarea type = "text" name="desc" placeholder="DESCRIPTION" value = {this.state.desc}></textarea>
+                </p>
+                <p><input type = "submit"></input></p>
+            </form>
         )
     }
 }
--- src/containers/Update.jsx
+++ src/containers/Update.jsx
@@ -1,4 +1,25 @@
 import { connect } from 'react-redux';
 import Update from '../components/Update'
 
-export default connect()(Update);
+export default connect(
+    function(state){
+        let title,desc,id;
+        for(let i =0;i<state.contents.length;i++){
+            let d = state.contents[i];
+            if(d.id===state.selected_content_id){
+                title = d.title;
+                desc = d.desc;
+                id = d.id;
+                break;
+            }
+        }
+        return{
+            title,
+            desc,
+            id,
+        }
+    },
+    function(dispatch){
+        return{}
+    },
+)(Update);
