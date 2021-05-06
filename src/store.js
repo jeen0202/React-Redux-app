a4455d1fa3b726b5fc0910680341ca8939ae30fc
sejing <jeen0202@korea.ac.kr>
2021-05-06 16:02:46 +0900

    데이터를 Json 파일로 분리

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import {createStore} from 'redux';
+import List from "./list.json";
 const initState = {
     mode:'WELCOME',
     welcome_content:{
@@ -7,11 +8,12 @@ const initState = {
     },
     selected_content_id:1,
     max_content_id:3,
-    contents:[
-        {id:1, title:'HTML', desc:'HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.'},
-        {id:2, title:'CSS', desc:'CSS is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.'},
-        {id:3, title:'JS', desc:'JS is a programming language that conforms to the ECMAScript specification.'}
-    ]
+    contents:List.contents,
+    // contents:[
+    //     {id:1, title:'HTML', desc:'HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.'},
+    //     {id:2, title:'CSS', desc:'CSS is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.'},
+    //     {id:3, title:'JS', desc:'JS is a programming language that conforms to the ECMAScript specification.'}
+    // ]
     
 }
 function Reducer(state=initState,action){
