54c3a180a556f0352edb666d7e9b7e26dffa597c
Sejin Kim <jeen0202@korea.ac.kr>
2021-05-12 23:36:34 +0900

    Cotrol 표시 변경 및 hover 추가

--- src/components/Control.jsx
+++ src/components/Control.jsx
@@ -5,29 +5,26 @@ export default class Control extends Component {
     render()
     {
         return (
-            <ul>
-                <li>
+            <pre>                
                 <input className="controlButton" type="button" value = "CREATE" onClick={
                     function(e){
                         e.preventDefault();
                         this.props.onClick('CREATE');
                     }.bind(this)}></input>
-                </li>
-                <li>
+                <br></br>                
                 <input className="controlButton" type="button" value = "UPDATE" onClick={
                     function(e){
                         e.preventDefault();
                         this.props.onClick('UPDATE');
                     }.bind(this)
                 }></input>
-                </li>
-                <li>
+                <br></br>                
                 <input className="controlButton" type="button" value = "DELETE" onClick={function(e){
                     e.preventDefault();
                     this.props.onClick('DELETE_PROCESS');
                 }.bind(this)}></input>
-                </li>
-            </ul>
+                
+            </pre>
                 
                 
 
